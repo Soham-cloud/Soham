711f03b7f521377d32f1bd78024b9eed17dfa2c5
Soham-cloud <63040651+Soham-cloud@users.noreply.github.com>
2020-04-27 16:49:38 +0530

    Add files via upload

---
+++ NewFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sample_code_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String  x = null;
+			     int var = null;
+                int y ;
+		        int a;
+                String z = null;
+				
+				//var xx=new String[0];
+				var yy=new int[]{};
+				var zz=new char[5];
+				//a=4+2;
+		        z="this is not null";		
+        }
+    }
+}
