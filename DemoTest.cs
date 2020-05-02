aca26f09c6d6a57712e49f7e4baa33b8fc74326f
Soham-cloud <63040651+Soham-cloud@users.noreply.github.com>
2020-05-02 16:01:39 +0530

    Add files via upload

---
+++ DemoTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Sample_code_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String  x = null;
+             int y;
+		     int a;
+             String z = null;
+				//performing operation
+				var xx=new String[0];
+				var yy=new int[]{};
+				//var zz=new char[5];
+				a=4+2;
+		        z="this is not null";
+				/* y = 10;
+				   int b =0;*/
+					
+        }
+    }
+}
